c12ac492764a669fce04aef385ca73bfaa465ba2
DenisKokorin <144897255+DenisKokorin@users.noreply.github.com>
2024-11-30 14:46:27 +0300

    Update load.py

--- ML/load.py
+++ ML/load.py
@@ -6,20 +6,3 @@ def load_model():
     return model
 
 model = load_model()
-
-#results = model.predict(source="C://Users/denis/Downloads/1.jpg", conf=0.5)
-
-# res = {}
-#
-# for r in results:
-#     boxes = r.boxes.data.tolist()
-#     for box in boxes:
-#         res["xmin"] = box[0]
-#         res["ymin"] = box[1]
-#         res["xmax"] = box[2]
-#         res["ymax"] = box[3]
-#         res["confidence"] = box[4]
-#         res["class"] = box[5]
-#         res["class_name"] = model.names[int(box[5])]
-
-#print(res)
